645b189e4a4dbd2046aa999361b90d29dc334479
Hamamura-S <hamamurasantiago@frba.utn.edu.ar>
2024-05-04 14:46:54 -0300

    Implementado generarSugerencias en usuario

--- src/main/java/com/solution/Usuario.java
+++ src/main/java/com/solution/Usuario.java
@@ -2,13 +2,19 @@ package com.solution;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.Lists;
+import com.solution.enums.Categoria;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Usuario {
 
   protected List<Prenda> guardarropas;
   protected List<BorradorPrenda> borradoresPrendas;
   protected List<Atuendo> atuendos;
+  protected List<Sugerencia> sugerencias = new ArrayList<>();
 
   /**
    * La nueva instancia de borrador se retorna y ademas se guarda en el usuario.
@@ -47,6 +53,24 @@ public class Usuario {
 
   public void recibirAtuendo(Atuendo unAtuendo) {
     atuendos.add(unAtuendo);
+  }
+
+  public List<Sugerencia> generarSugerencias() {
+    List<Prenda> partesSuperiores = guardarropas.stream().filter(prenda -> prenda.getTipo().categoria == Categoria.PARTE_SUPERIOR).collect(Collectors.toList());
+    List<Prenda> partesInferiores = guardarropas.stream().filter(prenda -> prenda.getTipo().categoria == Categoria.PARTE_INFERIOR).collect(Collectors.toList());
+    List<Prenda> calzados = guardarropas.stream().filter(prenda -> prenda.getTipo().categoria == Categoria.CALZADO).collect(Collectors.toList());
+
+    List<List<Prenda>> cartesianProduct = Lists.cartesianProduct(partesSuperiores, partesInferiores, calzados);
+
+    List<Sugerencia> sugerencias = new ArrayList<>();
+    cartesianProduct.forEach(sugerencia -> sugerencias.add(
+        new Sugerencia(
+            sugerencia.get(0), //prenda superior
+            sugerencia.get(1), //prenda inferior
+            sugerencia.get(2)) //calzado
+    ));
+
+    return sugerencias;
   }
 
   //GETTERS
